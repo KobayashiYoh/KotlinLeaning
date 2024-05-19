debd1d22a847bdd74ef3390af45a27f21008ae59
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-05-20 00:29:59 +0900

    じゃんけんの基本的なロジックを実装

--- Janken/app/src/main/java/com/example/janken/ResultActivity.kt
+++ Janken/app/src/main/java/com/example/janken/ResultActivity.kt
@@ -8,6 +8,11 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.janken.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
+    private val gu = 0
+    private val choki = 1
+    private val pa = 2
+
     private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +21,37 @@ class ResultActivity : AppCompatActivity() {
         setContentView(binding.root)
         val id = intent.getIntExtra("MY_HAND", 0)
 
-        when(id) {
-            R.id.gu -> binding.myHandImage.setImageResource(R.drawable.gu)
-            R.id.choki -> binding.myHandImage.setImageResource(R.drawable.choki)
-            R.id.pa -> binding.myHandImage.setImageResource(R.drawable.pa)
+        val myHand: Int = when(id) {
+            R.id.gu -> {
+                binding.myHandImage.setImageResource(R.drawable.gu)
+                gu
+            }
+            R.id.choki -> {
+                binding.myHandImage.setImageResource(R.drawable.choki)
+                choki
+            }
+            R.id.pa -> {
+                binding.myHandImage.setImageResource(R.drawable.pa)
+                pa
+            }
+            else -> gu
         }
+
+        val comHand = (Math.random() * 3).toInt()
+        when(comHand) {
+            gu -> binding.comHandImage.setImageResource(R.drawable.com_gu)
+            choki -> binding.comHandImage.setImageResource(R.drawable.com_choki)
+            pa -> binding.comHandImage.setImageResource(R.drawable.com_pa)
+        }
+
+        val gameResult = (comHand -myHand + 3) % 3
+
+        when(gameResult) {
+            0 -> binding.resultLabel.setText(R.string.result_draw)
+            1 -> binding.resultLabel.setText(R.string.result_win)
+            2 -> binding.resultLabel.setText(R.string.result_lose)
+        }
+        binding.backButton.setOnClickListener { finish() }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
