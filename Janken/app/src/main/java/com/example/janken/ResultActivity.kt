8235f064e076edf22bd7e1f0df34f583ef073b60
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-05-20 00:09:40 +0900

    画面遷移の値渡しを実装

--- Janken/app/src/main/java/com/example/janken/ResultActivity.kt
+++ Janken/app/src/main/java/com/example/janken/ResultActivity.kt
@@ -5,13 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.janken.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val id = intent.getIntExtra("MY_HAND", 0)
+
+        when(id) {
+            R.id.gu -> binding.myHandImage.setImageResource(R.drawable.gu)
+            R.id.choki -> binding.myHandImage.setImageResource(R.drawable.choki)
+            R.id.pa -> binding.myHandImage.setImageResource(R.drawable.pa)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
