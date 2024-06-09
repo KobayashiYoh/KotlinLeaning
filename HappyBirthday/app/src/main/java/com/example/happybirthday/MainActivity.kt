e0bbe38bce05a0e32afb5e1bf865df911a9478c1
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-06-09 14:30:42 +0900

    Revert "feat: GreetingPreviewの背景色を設定"
    This reverts commit 34c4506924c0afb13ca631e6572092a0ca5910fe.

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -44,8 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        Surface(color = Color.Magenta) {
-            Greeting("Android")
-        }
+        Greeting("Android")
     }
 }
